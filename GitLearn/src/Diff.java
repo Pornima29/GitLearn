ce2113621373c9a51bc87be833410551a49b25ef
Pornima Kanchangire <shalinikanchangire1@gmail.com>
2022-11-15 18:08:32 +0530

    Modified Sun.java & Adding Diff.java

---
+++ GitLearn/src/Diff.java
@@ -0,0 +1,9 @@
+
+public class Diff {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- GitLearn/src/Sum.java
+++ GitLearn/src/Sum.java
@@ -5,8 +5,9 @@ public class Sum {
 		int a =2;
 		int b = 3;
 		int c = a+b;
-	System.out.println(c);
+	System.out.println("Value of Variable c :"+c);
 
+	
 	}
 
 }
