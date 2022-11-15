8dacd7dbba3f891a9f4cf7ce8e04c7b71664487b
Pornima Kanchangire <shalinikanchangire1@gmail.com>
2022-11-15 18:35:05 +0530

    Add Variable & Printing Statment In Sum.java

--- GitLearn/src/Sum.java
+++ GitLearn/src/Sum.java
@@ -4,8 +4,11 @@ public class Sum {
 	public static void main(String[] args) {
 		int a =2;
 		int b = 3;
+		int d= 1;
 		int c = a+b;
+		int e = a+b+d;
 	System.out.println("Value of Variable c :"+c);
+	System.out.println(e);
 
 	
 	}
